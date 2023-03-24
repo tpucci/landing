ddc024733d7953e0f22bb6705e00cdeb61f8872f
Thomas Pucci <thomas.lb.pucci@gmail.com>
2023-03-24 23:48:33 +0100

    feat: try things

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <section class="bg-gradient-to-b md:bg-gradient-to-r from-white via-purple-50 to-sky-100 dark:from-slate-900 dark:via-purple-900/10 dark:to-sky-900/20 mt-[-72px]">
+    <section class="bg-gradient-to-b md:bg-gradient-to-r from-sky-100 via-purple-50/10 to-white/20 dark:from-slate-900 dark:via-purple-900/10 dark:to-sky-900/20 mt-[-72px]">
       <div class="max-w-6xl mx-auto px-4 sm:px-6 md:flex md:h-screen 2xl:h-auto pt-[72px]">
         <div class="py-12 md:py-12 lg:py-16 block md:flex text-center md:text-left">
           <div class="pb-12 md:pb-0 md:py-0 mx-auto md:pr-16 flex items-center basis-3/5">
