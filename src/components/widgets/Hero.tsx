311a4999622866db593cdbdc7c30ac799e338c1c
Thomas Pucci <thomas.lb.pucci@gmail.com>
2023-04-02 21:10:43 +0200

    feat: make light mode easier to read

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -5,20 +5,20 @@ export default component$(() => {
   return (
     <div class="relative">
       <Perspective />
-      <section class="bg-gradient-to-b md:bg-gradient-to-r from-sky-100 via-purple-50/10 to-white/20 dark:from-slate-900 dark:via-purple-900/10 dark:to-sky-900/20 mt-[-72px] relative">
+      <section class="bg-gradient-to-b md:bg-gradient-to-r from-sky-100/90 via-purple-50/20 to-transparent dark:from-slate-900 dark:via-purple-900/10 dark:to-sky-900/20 mt-[-72px] relative">
         <div class="max-w-6xl mx-auto px-4 sm:px-6 md:flex md:h-screen 2xl:h-auto pt-[72px]">
           <div class="py-12 md:py-12 lg:py-16 block md:flex text-center md:text-left">
             <div class="pb-12 md:pb-0 md:py-0 mx-auto md:pr-16 flex items-center basis-3/5">
               <div>
                 <h1 class="text-5xl md:text-[3.48rem] font-bold leading-tighter tracking-tighter mb-4 font-heading px-4 md:px-0">
                   <span>Intelligible</span>{" "}
-                  <span class="text-primary-500">Roadmap</span>{" "}
+                  <span class="text-primary-500 drop-shadow-lg">Roadmap</span>{" "}
                   <span>to align the team</span>
                   <br class="hidden lg:block" />{" "}
                   <span class="hidden lg:inline">with the plan</span>
                 </h1>
                 <div class="max-w-3xl mx-auto">
-                  <p class="text-xl text-gray-600 mb-8 dark:text-slate-400">
+                  <p class="text-xl text-slate-800 mb-8 dark:text-slate-400">
                     <span class="font-semibold underline decoration-wavy decoration-1 decoration-primary-600 underline-offset-2">
                       Cadence
                     </span>{" "}
